8e7bea71e45990a567211999a6057cf847eb1408
ROY <101548778+Nightcatclub@users.noreply.github.com>
2023-05-18 22:25:46 +1000

    upload of the pet form
    Complete the upload of the pet form and solved the problem of server error reporting

--- public/js/script-user-info.js
+++ public/js/script-user-info.js
@@ -67,6 +67,81 @@ document
       });
   });
 
+// pet information
+// Add img
+let petimgUrl;
+fetch("/api/petinfo")
+  .then((response) => response.json())
+  .then((data) => {
+    const petInfo = data.data[0];
+    if (petInfo && petInfo.photo) {
+      document.querySelector("#pet-photo-preview").src = petInfo.photo;
+    }
+  });
+document
+  .getElementById("pet-upload-button")
+  .addEventListener("click", function () {
+    document.getElementById("pet-photo").click();
+  });
+document
+  .getElementById("pet-photo")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      document.getElementById("pet-photo-preview").src = reader.result;
+      // send file to server
+      const formData = new FormData();
+      formData.append("pet-photo-img", file);
+
+      fetch("/api/pet-upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          petimgUrl = data.imgUrl;
+          document.querySelector(".pet-photo-img").src = petimgUrl;
+        })
+        .catch((error) => console.error(error));
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  });
+
+// submit pet imfomation
+document
+  .querySelector("#pet-submit-btn")
+  .addEventListener("click", function () {
+    const petInfo = {
+      petName: document.querySelector("#pet-name").value,
+      petType: document.querySelector("#pet-type").value,
+      petBreed: document.querySelector("#pet-breed").value,
+      petAge: document.querySelector("#pet-age").value,
+      petGender: document.querySelector("#pet-gender").value,
+      petWeight: document.querySelector("#pet-weight").value,
+      photo: petimgUrl,
+    };
+
+    fetch("/api/petinfo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(petInfo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Pet info:", data);
+        alert("Pet information has been successfully stored.");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+
 function submitClick() {
   alert("Thanks for Submit information!");
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+// Route for the root URL
 app.use(express.static(__dirname + "/public"));
 app.use("/uploads", express.static("uploads"));
 app.get("/", function (req, res) {
@@ -17,8 +17,9 @@ app.get("/", function (req, res) {
 
 client.connect((err) => {
   if (err) throw err;
-  userCollection = client.db("User-information").collection("userInfo");
-  petCollection = client.db("User-information").collection("petInfo");
+  // Get the 'userInfo' and 'petInfo' collections from the database
+  userCollection = client.db("GroupProject").collection("userInfo");
+  petCollection = client.db("GroupProject").collection("petInfo");
   console.log("Connected to MongoDB!");
 });
 
@@ -35,56 +36,7 @@ let userCollection, petCollection;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/api/userinfo", (req, res) => {
-  const userInfo = req.body;
-  userCollection.insertOne(userInfo, (err, result) => {
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({
-        statusCode: 200,
-        data: result,
-        message: "Successfully added",
-      });
-    }
-  });
-});
-
-app.post("/api/petinfo", (req, res) => {
-  const petInfo = req.body;
-  petCollection.insertOne(petInfo, (err, result) => {
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({
-        statusCode: 200,
-        data: result,
-        message: "Successfully added",
-      });
-    }
-  });
-});
-
-app.get("/api/userinfo", (req, res) => {
-  userCollection.find().toArray((err, result) => {
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({ statusCode: 200, data: result, message: "Successful" });
-    }
-  });
-});
-
-app.get("/api/petinfo", (req, res) => {
-  petCollection.find().toArray((err, result) => {
-    if (err) {
-      res.json({ statusCode: 400, message: err });
-    } else {
-      res.json({ statusCode: 200, data: result, message: "Successful" });
-    }
-  });
-});
-
+// User photo upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -102,6 +54,14 @@ app.post("/api/upload", upload.single("user-info-img"), (req, res) => {
   res.json({ message: "File uploaded successfully", imgUrl: imgUrl });
 });
 
+// Pet photo upload
+app.post("/api/pet-upload", upload.single("pet-photo-img"), (req, res) => {
+  console.log(req.file); // contains information about the file.
+  const petimgUrl = "/uploads/" + req.file.filename;
+  res.json({ message: "Pet image uploaded successfully", imgUrl: petimgUrl });
+});
+
+// Store user info
 app.post("/api/userinfo", (req, res) => {
   const userInfo = req.body;
   userCollection.insertOne(userInfo, (err, result) => {
@@ -113,6 +73,44 @@ app.post("/api/userinfo", (req, res) => {
         data: result,
         message: "Successfully added",
       });
+    }
+  });
+});
+
+// Store pet info
+app.post("/api/petinfo", (req, res) => {
+  const petInfo = req.body;
+  petCollection.insertOne(petInfo, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err });
+    } else {
+      res.json({
+        statusCode: 200,
+        data: result,
+        message: "Successfully added",
+      });
+    }
+  });
+});
+
+// Fetch user info
+app.get("/api/userinfo", (req, res) => {
+  userCollection.find().toArray((err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err });
+    } else {
+      res.json({ statusCode: 200, data: result, message: "Successful" });
+    }
+  });
+});
+
+// Fetch pet info
+app.get("/api/petinfo", (req, res) => {
+  petCollection.find().toArray((err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err });
+    } else {
+      res.json({ statusCode: 200, data: result, message: "Successful" });
     }
   });
 });
