ca65a6764a5616e02829f4df12175fab5102f6dc
DongqiShen96 <118418221+DongqiShen96@users.noreply.github.com>
2023-05-18 23:20:22 +1000

    Connect to mangodb

---
+++ dbConnection.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.cxhciec.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let projectCollection;
+
+client.connect(err => {
+    
+    if (!err) {
+        console.log('Mongo DB connected');
+    } else {
+        console.log('Error: ', err);
+        process.exit(1);
+    }
+});
+
+module.exports = client;
