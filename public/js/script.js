5c23895bd504f65f036d2f961095a9a9e340e4f5
Trung M. Tran <trantrung951@gmail.com>
2023-05-25 18:27:30 +1000

    Email check
    Adding code for valid email checking

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,22 @@
 const submitForm = () => {
   let formData = {};
-  formData.email = $('#email').val();
+  let email1 = $('#email').val();
   let pw1 = $('#password').val();
   let pw2 = $('#confirm_password').val();
   
+  //email check
+  let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  if(!emailRegex.test(email1)) {
+    alert("Email is not valid");
+    return;
+  }
+
   //Pw check
   if (pw1 !== pw2) {
     alert("Not matched")
     return;
   }
-
+  formData.email = $('#email').val();
   formData.password = $('#password').val();
   // Print for test
   console.log('form data: ', formData);
