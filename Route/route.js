3f346c22cdb0802050e58b0f8d8f52b54b13b8a7
Trung M. Tran <trantrung951@gmail.com>
2023-05-25 14:36:48 +1000

    Route add for login

--- Route/route.js
+++ Route/route.js
@@ -2,6 +2,9 @@ var express = require('express');
 let router = express.Router();
 let controller = require('../controller/controller')
 
-
+// this route is for adding calculation history to db.
+router.post('/api/login', (req, res) => {
+    controller.createHistory(req, res);
+});
 
 module.exports = router;
