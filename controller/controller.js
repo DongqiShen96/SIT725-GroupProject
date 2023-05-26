5dffe3e57b6fc30fb48b53d4123700a8afcf145f
Trung M. Tran <trantrung951@gmail.com>
2023-05-26 14:01:46 +1000

    Sign-up: Hashing password and used email checking for sign-up

--- controller/controller.js
+++ controller/controller.js
@@ -4,14 +4,14 @@ var bcrypt = require('bcryptjs');
 const createUser = (req,res) => {
     let user = req.body;
     let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(formData.password, salt);
+    let hash = bcrypt.hashSync(user.password, salt);
     user.password = hash;
 
     //If not exist, add user
-    model.checkUserExists(user.email, (exists) => {
+    model.checkUser(user.email, (exists) => {
         if (exists) {
             res.json({statusCode: 400, message: 'You already signed up'});
-        } else {
+        } else {``
             model.createUser(user, (err, result) => {
                 if (err) {
                     res.json({statusCode: 400, message: err});
